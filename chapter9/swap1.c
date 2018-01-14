2f7d15f80ffb5f97adfe494f9def7ae8b0f4c512
cs <csleleljl@gmail.com>
2018-01-14 13:31:02 +0800

    finish chapter 9

---
+++ chapter9/swap1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void interchange(int u, int v);
+
+void interchange2(int *, int *);
+
+int main(void)
+{
+    int x = 5, y = 10;
+    printf("Originally x = %d and y = %d.\n", x, y);
+    interchange(x, y);
+    interchange2(&x, &y);
+    printf("Now x = %d and y = %d.\n", x, y);
+    return 0;
+}
+
+void interchange(int u, int v)
+{
+    int temp;
+    temp = u;
+    u = v;
+    v = temp;
+}
+
+void interchange2(int *u, int *v)
+{
+    int temp;
+    temp = *u;
+    *u = *v;
+    *v = temp;
+}
